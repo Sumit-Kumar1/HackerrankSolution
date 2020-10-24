8da637b88fc8e838a82774a7470cbebbc919c5ce
Aryan Srivastava <65230360+aryan3108@users.noreply.github.com>
2020-10-24 11:16:25 -0700

    Update ifElse.py

--- Python/ifElse.py
+++ Python/ifElse.py
@@ -1,12 +1,12 @@
 #!/bin/python3
 if __name__ == '__main__':
-    n = int(input().strip())
-    if n%2 != 0:
+    a = int(input().strip())#taking input
+    if a%2 != 0:
         print("Weird")
     else:
-        if n >= 2 and n <= 5:
+        if a >= 2 and a <= 5:
             print("Not Weird")
-        elif n >= 6 and n <= 20:
+        elif a >= 6 and a <= 20:
             print("Weird")
         else:
             print("Not Weird")
